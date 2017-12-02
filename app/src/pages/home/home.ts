f2989b6bdc581526a0bb712d5b95882ce6c5b2e3
James <jamesleahy314@gmail.com>
2017-12-01 23:49:59 -0800

    add images and marker

--- app/src/pages/home/home.ts
+++ app/src/pages/home/home.ts
@@ -12,6 +12,9 @@ import {
 } from '@ionic-native/google-maps';
 
 import { TransportProvider } from '../../providers/transport/transport'
+import { LoadingController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { ChangeDetectorRef } from '@angular/core';
 
 declare var google;
 
@@ -25,12 +28,17 @@ export class HomePage {
 
   @ViewChild('map') mapElement: ElementRef;
   map: any;
-  latitude: any;
-  longitude: any;
+  latitude: string="5";
+  longitude: string= "5";
 
 
-
-  constructor(public platform: Platform, private transport: TransportProvider, private googleMaps: GoogleMaps) {
+  constructor(
+    public platform: Platform, 
+    private transport: TransportProvider, 
+    private googleMaps: GoogleMaps, 
+    public loadingCtrl: LoadingController, 
+    public alertCtrl: AlertController, 
+    private ref: ChangeDetectorRef) {
   }
 
 
@@ -40,35 +48,93 @@ export class HomePage {
 
   initializeMap() {
     this.platform.ready().then(() => {
+      // let position;
       let minZoomLevel = 5;
       let mapOptions = {
         zoom: minZoomLevel,
-        center: new google.maps.LatLng(38.50, -90.50),
+        center: new google.maps.LatLng(37.39, -119),
         mapTypeId: google.maps.MapTypeId.TERRIAN
       }
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-      var position = new google.maps.LatLng("48.4283", "123.3649");
-      var dogwalkMarker = new google.maps.Marker({ position: position, title: "Testing" });
-      dogwalkMarker.setMap(this.map);
+      // Place a draggable marker on the map
+      let marker = new google.maps.Marker({
+        map: this.map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: {lat: 37.39, lng: -119}
+      });
+      marker.addListener('drag', ()=> {
+       
+        this.latitude = marker.position.lat();
+        this.longitude = marker.position.lng();
+        console.log("lat: " + this.latitude + " long: " + this.longitude)
+        this.ref.detectChanges();
+      });
     });
 
   }
 
   sampleHttpRequest() {
-    let body = {}
+    console.log("lat: " + parseInt(this.latitude, 10) + " long: " + parseInt(this.longitude, 10))
+    let body = { "year": 1980, "latitude": parseInt(this.latitude, 10), "longitude": parseInt(this.longitude, 10) }
+    
+    let loader = this.loadingCtrl.create({
+      content: "Prediciting...",
+    });
+    loader.present();
 
-    this.transport.get('tree').then((data) => {
-      console.log("here is the model: " + data)
-    },
-      (error) => {
-        console.log("Error occur: " + error);
-      })
 
-    this.transport.post('examplePost', body).then((data) => {
+    this.transport.post('predict', body).then((data) => {
       console.log("here is the post: " + data)
+      let myTitle = this.getTitle(data)
+      let mySubTitle = this.getSubTitle(data)
+      loader.dismiss().then(()=> {
+        let alert = this.alertCtrl.create({
+          title: myTitle,
+          subTitle: mySubTitle,
+          buttons: ['OK']
+        });
+        alert.present();
+      })
     },
       (error) => {
         console.log("Error occurs: " + error);
+        loader.dismiss()
       })
   }
+
+  getTitle(data) {
+    switch(data) {
+      case 'A':
+        return "No Fire"
+      case 'B':
+        return "Smouldering Ground Fire"
+      case 'C':
+        return "Low Vigour Surface Fire"
+      case 'D':
+        return "Moderately Vigorous Surface Fire"
+      default: 
+        return "Passive Crown Fire"
+    }
+  }
+
+  getSubTitle(data) {
+    switch(data) {
+      case 'A':
+        return "A fire was not found"
+      case 'B':
+        return "White smoke, no open flame"
+      case 'C':
+        return "Smoke, visible open flame"
+      case 'D':
+        return "Grey to black smoke, Organized surface flame"
+      default:
+        return "Possible fire balls and whirls, Dominate smoke colum"
+    }
+  }
+
+  ok() {
+    console.log ("ok")
+  }
+
 }
